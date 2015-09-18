8cd4764aa3e0cb4b57d7c02c575deaaf2704acea
kadlabalu <kadlabalu.misc@gmail.com>
2015-09-18 16:13:14 +0530

    Added userId tracking snippet as per GA doc.

--- js/dx.js
+++ js/dx.js
@@ -16,6 +16,7 @@
   	
   	//Track this user in google analytics
 	ga('set', 'dimension2', userId);
+	ga('set', 'dimension3', userId);
 	ga('send', 'pageview');
 
   	// If mixpanel is available, track the user there along with any campaign information we may have.
